efbe0a0cb4929892e9a822568bcdf5830634928d
mooncrown04 <mooncrown04@gmail.com>
2026-03-28 22:06:15 +0300

    Update script.py

--- script.py
+++ script.py
@@ -13,7 +13,6 @@ TIMEOUT = 5
 
 # M3U kaynak linkleri (Buraya güncel linkleri ekleyebilirsiniz)
 M3U_SOURCES = [
-    //'https://raw.githubusercontent.com/GitLatte/patr0n/refs/heads/site/lists/iptvsevenler.m3u',
     'https://raw.githubusercontent.com/Mertcantv/Mertcan/refs/heads/main/%C4%B0zle2.m3u',
     'https://raw.githubusercontent.com/primatzeka/kurbaga/main/NeonSpor/NeonSpor.m3u',
     'https://tinyurl.com/TVCANLI'
